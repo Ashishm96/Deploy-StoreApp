38ca530e470f22d15f910f2a5acadf1b18b9734b
Ashishm96 <ashishmakasare24@gmail.com>
2024-08-19 00:26:28 +0530

    update app.js

--- store-app/app.js
+++ store-app/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Good Morning, This Website is created by Ashish');
+  res.send('Good Morning, This Website is created by Ashish makasare want to add more things');
 });
 
 app.listen(port, () => {
